ae25fceea5b1e2790c752efa38b5c6ca987e1593
gregyjames <madra.uc@gmail.com>
2017-03-08 01:15:47 -0600

    Added Files

---
+++ OctaneTester/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OctaneDownloadEngine;
+
+namespace OctaneDownloadEngine
+{
+    class Program
+    {
+        static void Main()
+        {
+            var Engine = new OctaneEngine();
+            Engine.SplitDownload("http://www.hdwallpapers.in/walls/tree_snake_hd-wide.jpg", "output.jpg", 2);
+
+            
+            Console.ReadLine();
+        }
+    }
+}
