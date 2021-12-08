c33b3446d893867dfca34369158c8196d0827377
Greg James <gregvjames99@gmail.com>
2021-12-07 20:23:31 -0600

    Cleaned up CodeQL Warnings

--- OctaneEngine/Engine.cs
+++ OctaneEngine/Engine.cs
@@ -96,6 +96,100 @@ namespace OctaneEngine
                 {
                     using (var pbar = new ProgressBar(parts, "Downloading File...", options))
                     {
+                        try
+                        {
+                            await Parallel.ForEachAsync(pieces,
+                                new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount },
+                                async (piece, cancellationToken) =>
+                                {
+                                    //Get a http client from the pool and request for the content range
+                                    var client = httpPool.Get();
+                                    var request = new HttpRequestMessage { RequestUri = new Uri(url) };
+                                    request.Headers.Range = new RangeHeaderValue(piece.Start, piece.End);
+
+                                    //Request headers so we dont cache the file into memory
+                                    if (client != null)
+                                    {
+                                        var message = await client.SendAsync(request,
+                                            HttpCompletionOption.ResponseHeadersRead,
+                                            cancellationToken).ConfigureAwait(false);
+
+                                        if (message.IsSuccessStatusCode)
+                                        {
+                                            //Get the content stream from the message request
+                                            using (var streamToRead = await message.Content
+                                                .ReadAsStreamAsync(cancellationToken)
+                                                .ConfigureAwait(false))
+                                            {
+                                                //Create a memory mapped stream to the mmf with the piece offset and size equal to the response size
+                                                using (var streams = mmf.CreateViewStream(piece.Start,
+                                                    message.Content.Headers.ContentLength!.Value,
+                                                    MemoryMappedFileAccess.ReadWrite))
+                                                {
+                                                    using (var chil =
+                                                        pbar.Spawn(
+                                                            (int)Math.Round(
+                                                                (decimal)(message.Content.Headers.ContentLength /
+                                                                          bufferSize)), "", childOptions))
+                                                    {
+                                                        //Copy from the content stream to the mmf stream
+                                                        var buffer = new byte[bufferSize];
+                                                        int offset, bytesRead;
+                                                        // Until we've read everything
+                                                        do
+                                                        {
+                                                            offset = 0;
+                                                            // Until the buffer is very nearly full or there's nothing left to read
+                                                            do
+                                                            {
+                                                                bytesRead = await streamToRead.ReadAsync(
+                                                                    buffer.AsMemory(offset, bufferSize - offset),
+                                                                    cancellationToken).ConfigureAwait(false);
+                                                                offset += bytesRead;
+                                                            } while (bytesRead != 0 && offset < bufferSize);
+
+                                                            // Empty the buffer
+                                                            if (offset != 0)
+                                                            {
+                                                                //fileStrm.Write(buffer, 0, offset);
+                                                                await streams.WriteAsync(buffer.AsMemory(0, offset),
+                                                                    cancellationToken).ConfigureAwait(false);
+                                                                chil.Tick();
+                                                            }
+                                                        } while (bytesRead != 0);
+                                                    }
+
+                                                    streams.Flush();
+                                                    streams.Close();
+                                                }
+
+                                                streamToRead.Close();
+                                            }
+                                        }
+
+                                        pbar.Tick();
+                                        message.Dispose();
+                                    }
+
+                                    request.Dispose();
+                                });
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        finally
+                        {
+                            mmf.Dispose();
+                            httpPool.Empty();
+                        }
+                    }
+                }
+
+                else
+                {
+                    try
+                    {
                         await Parallel.ForEachAsync(pieces,
                             new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount },
                             async (piece, cancellationToken) =>
@@ -124,38 +218,30 @@ namespace OctaneEngine
                                                 message.Content.Headers.ContentLength!.Value,
                                                 MemoryMappedFileAccess.ReadWrite))
                                             {
-                                                using (var chil =
-                                                    pbar.Spawn(
-                                                        (int)Math.Round(
-                                                            (decimal)(message.Content.Headers.ContentLength /
-                                                                      bufferSize)), "", childOptions))
+                                                //Copy from the content stream to the mmf stream
+                                                var buffer = new byte[bufferSize];
+                                                int offset, bytesRead;
+                                                // Until we've read everything
+                                                do
                                                 {
-                                                    //Copy from the content stream to the mmf stream
-                                                    var buffer = new byte[bufferSize];
-                                                    int offset, bytesRead;
-                                                    // Until we've read everything
+                                                    offset = 0;
+                                                    // Until the buffer is very nearly full or there's nothing left to read
                                                     do
                                                     {
-                                                        offset = 0;
-                                                        // Until the buffer is very nearly full or there's nothing left to read
-                                                        do
-                                                        {
-                                                            bytesRead = await streamToRead.ReadAsync(
-                                                                buffer.AsMemory(offset, bufferSize - offset),
-                                                                cancellationToken).ConfigureAwait(false);
-                                                            offset += bytesRead;
-                                                        } while (bytesRead != 0 && offset < bufferSize);
+                                                        bytesRead = await streamToRead.ReadAsync(
+                                                            buffer.AsMemory(offset, bufferSize - offset),
+                                                            cancellationToken);
+                                                        offset += bytesRead;
+                                                    } while (bytesRead != 0 && offset < bufferSize);
 
-                                                        // Empty the buffer
-                                                        if (offset != 0)
-                                                        {
-                                                            //fileStrm.Write(buffer, 0, offset);
-                                                            await streams.WriteAsync(buffer.AsMemory(0, offset),
-                                                                cancellationToken).ConfigureAwait(false);
-                                                            chil.Tick();
-                                                        }
-                                                    } while (bytesRead != 0);
-                                                }
+                                                    // Empty the buffer
+                                                    if (offset != 0)
+                                                    {
+                                                        //fileStrm.Write(buffer, 0, offset);
+                                                        await streams.WriteAsync(buffer.AsMemory(0, offset),
+                                                            cancellationToken);
+                                                    }
+                                                } while (bytesRead != 0);
 
                                                 streams.Flush();
                                                 streams.Close();
@@ -165,88 +251,22 @@ namespace OctaneEngine
                                         }
                                     }
 
-                                    pbar.Tick();
                                     message.Dispose();
                                 }
 
                                 request.Dispose();
+                                httpPool.Return(client);
                             });
-                        mmf.Dispose();
-                        httpPool.Empty();
                     }
-                }
-
-                else
-                {
-                    await Parallel.ForEachAsync(pieces,
-                        new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount },
-                        async (piece, cancellationToken) =>
-                        {
-                            //Get a http client from the pool and request for the content range
-                            var client = httpPool.Get();
-                            var request = new HttpRequestMessage { RequestUri = new Uri(url) };
-                            request.Headers.Range = new RangeHeaderValue(piece.Start, piece.End);
-
-                            //Request headers so we dont cache the file into memory
-                            if (client != null)
-                            {
-                                var message = await client.SendAsync(request,
-                                    HttpCompletionOption.ResponseHeadersRead,
-                                    cancellationToken).ConfigureAwait(false);
-
-                                if (message.IsSuccessStatusCode)
-                                {
-                                    //Get the content stream from the message request
-                                    using (var streamToRead = await message.Content
-                                        .ReadAsStreamAsync(cancellationToken)
-                                        .ConfigureAwait(false))
-                                    {
-                                        //Create a memory mapped stream to the mmf with the piece offset and size equal to the response size
-                                        using (var streams = mmf.CreateViewStream(piece.Start,
-                                            message.Content.Headers.ContentLength!.Value,
-                                            MemoryMappedFileAccess.ReadWrite))
-                                        {
-                                            //Copy from the content stream to the mmf stream
-                                            var buffer = new byte[bufferSize];
-                                            int offset, bytesRead;
-                                            // Until we've read everything
-                                            do
-                                            {
-                                                offset = 0;
-                                                // Until the buffer is very nearly full or there's nothing left to read
-                                                do
-                                                {
-                                                    bytesRead = await streamToRead.ReadAsync(
-                                                        buffer.AsMemory(offset, bufferSize - offset),
-                                                        cancellationToken);
-                                                    offset += bytesRead;
-                                                } while (bytesRead != 0 && offset < bufferSize);
-
-                                                // Empty the buffer
-                                                if (offset != 0)
-                                                {
-                                                    //fileStrm.Write(buffer, 0, offset);
-                                                    await streams.WriteAsync(buffer.AsMemory(0, offset),
-                                                        cancellationToken);
-                                                }
-                                            } while (bytesRead != 0);
-                                            
-                                            streams.Flush();
-                                            streams.Close();
-                                        }
-
-                                        streamToRead.Close();
-                                    }
-                                }
-
-                                message.Dispose();
-                            }
-
-                            request.Dispose();
-                            httpPool.Return(client);
-                        });
-                    mmf.Dispose();
-                    httpPool.Dispose();
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        mmf.Dispose();
+                        httpPool.Dispose();
+                    }
                 }
             }
             catch (Exception ex)
