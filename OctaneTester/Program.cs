553a32f8654da0a1f4b55f974228a1b9b81fd09d
Greg <gregyjames@users.noreply.github.com>
2023-04-01 18:44:05 -0500

    Fixed callback error and added JSON appsettings configuration support (#86)

--- OctaneTester/Program.cs
+++ OctaneTester/Program.cs
@@ -1,5 +1,6 @@
-using System;
+using System.IO;
 using System.Threading;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using OctaneEngine;
 using OctaneEngineCore;
@@ -13,38 +14,34 @@ namespace OctaneTester
         private const string Url = "https://plugins.jetbrains.com/files/7973/281233/sonarlint-intellij-7.4.0.60471.zip?updateId=281233&pluginId=7973&family=INTELLIJ";
         private static void Main()
         {
-            var config = new OctaneConfiguration
-            {
-                Parts = Environment.ProcessorCount / 2,
-                BufferSize = 2097152,
-                ShowProgress = true,
-                BytesPerSecond = 1,
-                UseProxy = false,
-                Proxy = null,
-                DoneCallback = _ => {  },
-                ProgressCallback = _ => {  },
-                NumRetries = 10
-            };
-            
-            //SERILOG EXAMPLE 
+            #region Logging Configuration
             var seriLog = new LoggerConfiguration()
                 .Enrich.FromLogContext()
                 .MinimumLevel.Fatal()
                 .WriteTo.File("./OctaneLog.txt")
                 .WriteTo.Console(theme: AnsiConsoleTheme.Sixteen)
                 .CreateLogger();
-
-            
             var factory = LoggerFactory.Create(logging =>
             {
                 logging.AddSerilog(seriLog);
             });
-            
+            #endregion
+
+            #region Configuration Loading
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true);
+            var configRoot = builder.Build();
+            var config = new OctaneConfiguration(configRoot, factory);
+            #endregion
+
+            #region Find and Set optimal number of parts
             var optimalNumberOfParts = Engine.GetOptimalNumberOfParts(Url).Result;
-            config.Parts = optimalNumberOfParts;
-            seriLog.Information($"Speed: {NetworkAnalyzer.GetCurrentNetworkSpeed().Result}");
-            seriLog.Information($"Latency: {NetworkAnalyzer.GetCurrentNetworkLatency().Result}");
-            seriLog.Information($"Optimal number of parts to download file: {optimalNumberOfParts}");
+            seriLog.Information("Optimal number of parts to download file: {OptimalNumberOfParts}", optimalNumberOfParts);
+            #endregion
+            
+            seriLog.Information("Speed: {Result}", NetworkAnalyzer.GetCurrentNetworkSpeed().Result);
+            seriLog.Information("Latency: {Result}", NetworkAnalyzer.GetCurrentNetworkLatency().Result);
             var pauseTokenSource = new PauseTokenSource();
             var cancelTokenSource = new CancellationTokenSource();
             Engine.DownloadFile(Url, factory, null, config, pauseTokenSource, cancelTokenSource).Wait(cancelTokenSource.Token);
